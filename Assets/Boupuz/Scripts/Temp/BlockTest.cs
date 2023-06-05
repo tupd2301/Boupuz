b28979ec23f7ac28e96b8ba3ee9e0a5dfa960ba2
tupd2301 <tupd@athena.studio>
2023-06-05 11:48:55 +0700

    Fix SpeedUp

--- Assets/Boupuz/Scripts/Temp/BlockTest.cs
+++ Assets/Boupuz/Scripts/Temp/BlockTest.cs
@@ -5,12 +5,47 @@ using UnityEngine;
 
 public class BlockTest : MonoBehaviour
 {
+    [SerializeField] private List<Vector3> _listDirection;
+    [SerializeField] private List<ContactPoint2D> _listContact;
+    [SerializeField] private List<Vector2> _listPosition;
+
+    private void Awake()
+    {
+        _listContact = new List<ContactPoint2D>();
+        _listDirection = new List<Vector3>();
+        _listPosition = new List<Vector2>();
+    }
 
     public void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.CompareTag("Ball"))
         {
-            BallController.Instance.CheckContact(coll.contacts[0], coll.gameObject);
+            Vector3 direction = coll.gameObject.GetComponent<BallModel>().Direction;
+            if (_listDirection.Where(a => a == direction).Count() > 0)
+            {
+                int index = _listDirection.IndexOf(direction);
+                float distance = Vector3.Distance(coll.transform.position, _listPosition[index]);
+                if (distance < 0.3f)
+                {
+                    Debug.Log("yub");
+                    coll.transform.position = _listPosition[index];
+                    BallController.Instance.CheckContact(_listContact[index], coll.gameObject);
+                }
+                else
+                {
+                    _listDirection.Add(direction);
+                    _listPosition.Add(coll.transform.position);
+                    _listContact.Add(coll.contacts[0]);
+                    BallController.Instance.CheckContact(coll.contacts[0], coll.gameObject);
+                }
+            }
+            else
+            {
+                _listDirection.Add(direction);
+                _listPosition.Add(coll.transform.position);
+                _listContact.Add(coll.contacts[0]);
+                BallController.Instance.CheckContact(coll.contacts[0], coll.gameObject);
+            }
         }
     }
 }
