e2ccd295a8931caa8d69850d361d918d2a1fe12b
tupd2301 <tupd@athena.studio>
2023-06-06 13:07:10 +0700

    update ballcontroller

--- Assets/Boupuz/Scripts/GameplayTest/Ball/BallController.cs
+++ Assets/Boupuz/Scripts/GameplayTest/Ball/BallController.cs
@@ -7,11 +7,14 @@ public class BallController : MonoBehaviour
 {
     public static BallController Instance;
     [SerializeField] private List<GameObject> _balls = new List<GameObject>();
+    [SerializeField] private List<BallModel> _listBallModel = new List<BallModel>();
     [SerializeField] private int _totalBall;
     [SerializeField] private float _speedToShoot;
     [SerializeField] private float _speedToRun;
     [SerializeField] private Vector3 _gunPosition;
     [SerializeField] private Vector2 _direction;
+    [SerializeField] private int _countBallRunnning;
+    [SerializeField] private GameObject _gun;
 
     public bool isShooted;
     public bool isEndRound;
@@ -23,14 +26,16 @@ public class BallController : MonoBehaviour
     public void GetBall(int amount)
     {
         isShooted = false;
-        _gunPosition = GameObject.Find("GunPosition").transform.position;
+        _gunPosition = _gun.transform.position;
         _totalBall = amount;
+        _xFirstBall = 0;
         _balls.AddRange(PoolManager.Instance.GetObjects("Ball", amount, transform));
         for (int i = 0; i < _totalBall; i++)
         {
-            _balls[i].GetComponentInChildren<BallModel>().Direction = Vector3.up;
+            _listBallModel.Add(_balls[i].GetComponentInChildren<BallModel>());
+            _listBallModel[i].Direction = Vector3.up;
             _balls[i].transform.position = new Vector3(_gunPosition.x, _gunPosition.y, 0);
-            _balls[i].GetComponentInChildren<BallModel>().IsRunning = false;
+            _listBallModel[i].IsRunning = false;
         }
     }
 
@@ -39,21 +44,21 @@ public class BallController : MonoBehaviour
     {
         isEndRound = false;
         isShooted = true;
+        _countBallRunnning = _totalBall;
         GameFlow.Instance.canShoot = false;
         GameFlow.Instance.timeScale = 1;
         if (_speedToShoot > 0)
         {
             for (int i = 0; i < _totalBall; i++)
             {
-                _balls[i].GetComponentInChildren<BallModel>().Direction = direction;
+                _listBallModel[i].Direction = direction;
                 //_balls[i].GetComponentInChildren<BallModel>().Direction = Vector3.up;
 
                 _balls[i].transform.position = new Vector3(_xFirstBall, _gunPosition.y, 0);
-                _balls[i].GetComponentInChildren<BallModel>().IsRunning = true;
+                _listBallModel[i].IsRunning = true;
                 yield return new WaitForSeconds(_speedToShoot * GameFlow.Instance.timeScale);
             }
         }
-        _xFirstBall = -10000;
     }
 
     public void CheckContact(ContactPoint2D contact, GameObject ball)
@@ -65,10 +70,11 @@ public class BallController : MonoBehaviour
 
     public void SetUpFirstBallReturned(float x)
     {
-        if (_xFirstBall == -10000)
+        if (_countBallRunnning == _totalBall)
         {
             GameFlow.Instance.ChangePositionJoystick(x);
             _xFirstBall = x;
+            Debug.Log("huyeah");
         }
     }
 
@@ -78,21 +84,22 @@ public class BallController : MonoBehaviour
         {
             for (int i = 0; i < _balls.Count; i++)
             {
-                if (_balls[i].activeInHierarchy && _balls[i].GetComponentInChildren<BallModel>().IsRunning)
+                if (_listBallModel[i].IsRunning)
                 {
-                    Vector3 direction = _balls[i].GetComponentInChildren<BallModel>().Direction;
+                    Vector3 direction = _listBallModel[i].Direction;
                     //_balls[i].transform.Translate(direction.normalized * _speedToRun * 0.01f * GameFlow.Instance.timeScale);
-                    _balls[i].transform.position = Vector3.MoveTowards(_balls[i].transform.position,(direction.normalized+_balls[i].transform.position),0.01f*_speedToRun);
-                    if (_balls[i].transform.position.y <= _gunPosition.y)
+                    _balls[i].transform.position = Vector3.MoveTowards(_balls[i].transform.position, (direction.normalized + _balls[i].transform.position), 0.01f * _speedToRun);
+                    if (_balls[i].transform.position.y <= _gunPosition.y && _balls[i].transform.position.x != _xFirstBall)
                     {
                         SetUpFirstBallReturned(_balls[i].transform.position.x);
+                        _countBallRunnning--;
                         _balls[i].transform.position = new Vector3(_xFirstBall, _gunPosition.y, _gunPosition.z) * GameFlow.Instance.timeScale;
-                        _balls[i].GetComponentInChildren<BallModel>().IsRunning = false;
+                        _listBallModel[i].IsRunning = false;
                     }
                 }
             }
         }
-        if (_balls.Where(ball => ball.GetComponentInChildren<BallModel>().IsRunning).Count() <= 0 && !isEndRound) // Scale up speed by time
+        if (_countBallRunnning <= 0 && !isEndRound) // Scale up speed by time
         {
             isEndRound = true;
             StopAllCoroutines();
