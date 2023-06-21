997a20bd267a07084f32c2c1b02465ab28225620
Ken <nghiavdng108@gmail.com>
2023-06-21 13:10:53 +0700

    brick destroyed by starvy will drop candy

--- Assets/Boupuz/Scripts/Brickie/BrickController.cs
+++ Assets/Boupuz/Scripts/Brickie/BrickController.cs
@@ -98,14 +98,7 @@ public class BrickController : MonoBehaviour
                 if (Data.Hp <= 0)
                 {
                     RemoveBrick();
-                    if (!gameObject.CompareTag("Item"))
-                    {
-                        GameBoardController.Instance.UpdateDestroyedBricks();
-                    }
-                    if (Data.hasCandy)
-                    {
-                        GameBoardController.Instance.UpdateCandy(1);
-                    }
+                    
 
 
                     if (Data.Id == 1 && Data.Type == ObjectType.Brickie) // if starvy
@@ -239,6 +232,7 @@ public class BrickController : MonoBehaviour
         if (Data.Hp <= value)
         {
             RemoveBrick();
+            
         }
         else
         {
@@ -251,7 +245,14 @@ public class BrickController : MonoBehaviour
     {
         gameObject.SetActive(false);
         GameBoardController.Instance.BrickControllers.Remove(this);
-
+        if (!gameObject.CompareTag("Item"))
+        {
+            GameBoardController.Instance.UpdateDestroyedBricks();
+        }
+        if (Data.hasCandy)
+        {
+            GameBoardController.Instance.UpdateCandy(1);
+        }
     }
 
     public void DecreaseAdjacentBrickHealth(BrickController brick)
